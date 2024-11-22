a24b943b54c308299a72daa601e26738b5df717d
saeraphas <saeraphas@users.noreply.github.com>
2024-11-22 13:06:19 -0500

    add url for menu settings

--- New-OpenShellMenuSetup.ps1
+++ New-OpenShellMenuSetup.ps1
@@ -3,7 +3,7 @@ $dir = "c:\temp"
 New-Item $dir -ItemType Directory -force
 $url = "https://github.com/Open-Shell/Open-Shell-Menu/releases/download/v4.4.191/OpenShellSetup_4_4_191.exe"
 $file = "$($dir)\OpenShellSetup_4_4_191.exe"
-$url2 = 
+$url2 = "https://raw.githubusercontent.com/saeraphas/open-shell-menu-setup/refs/heads/main/Menu_Settings.xml"
 $file2 = "$($dir)\Menu_Settings.xml"
 $argumentlist = @("/qn","ADDLOCAL=StartMenu")
 Invoke-WebRequest -Uri $url -outfile $file
